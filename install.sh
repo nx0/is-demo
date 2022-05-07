2250f02a52b1a6cd20f4eeb877d9300938806d40
francis <francisco.valero@edreamsodigeo.com>
2022-05-07 16:11:15 +0200

    holi

--- install.sh
+++ install.sh
@@ -1,24 +1,114 @@
 #!/bin/bash
 
-curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube-linux-amd64 /usr/local/bin/minikube
+# clean up conflictive env variables
+unset DOCKER_CERT_PATH
+unset DOCKER_TLS_VERIFY
+unset MINIKUBE_ACTIVE_DOCKERD
+unset DOCKER_HOST
 
+function endpoint_ready {
+    echo ""
+    echo ""
+    echo "  CONGRATULATIONS:"
+    echo "  ====================================================="
+    echo "  instant-search endpoint should be ready!!"
+    echo "  open a web browser and head to: http://localhost:8080"
+    echo ""
+    echo ""
+}
 
-cd packaging
-eval $(minikube docker-env)
-docker build -t instant-search .
-minikube image load instant-search:latest
-kubectl apply -f ckuster app.yml
+function upload_image {
+    echo "===> uploading image to minikube"
+    echo ""
+    echo "it may take a while, be patient :)"
+    echo ""
+    minikube image load instant-search:latest
+}
 
-# ensure using the context
-kubectl config use-context minikube (optional)
+function exit_grace {
+    echo "missing requirements, exiting..."
+    exit 1
+}
 
-(in an other terminal): minikube tunnel
+function install_minikube {
+    echo "===> installing minikube"
+    curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube_latest_amd64.deb
+    sudo dpkg -i minikube_latest_amd64.deb
+}
 
-# check if the endpoint is ready
-for i in $(seq 1 100); do
-    http=$(curl -IXGET -s http://localhost:8180)
-    if [ "$http" == "200" ]; then
-        open default browser: http://
+function install_kubectl {
+    echo "===> installing kubectl via snap"
+    sudo snap install kubectl --classic
+}
+
+echo ""
+echo "======== Detecting dependencies ========"
+echo ""
+echo -n "* checking docker installation ..."
+if docker -v >/dev/null; then
+    echo "ok"
+else
+    exit_grace
+fi
+echo -n "* checking if docker is running..."
+if [ "$(systemctl status docker | grep "Active:" | awk '{ print $3 }')" == "(running)" ]; then
+    echo "ok"
+else
+    exit_grace
+fi
+
+echo -n "* checking if minikube is installed..."
+if dpkg -l | grep minikube >/dev/null; then
+    echo "ok"
+else
+    echo "minikube not detected"
+    install_minikube
+fi
+
+echo -n "* checking kubectl..."
+if kubectl version --client 2>/dev/null; then
+    echo "ok"
+else
+    echo "WARNING: kubectl not found"
+    install_kubectl
+fi
+
+echo "* Detecting docker image of instant-search..."
+if ! docker image ls | grep instant-search >/dev/null; then
+    echo "WARNING: instant-search image not found..."
+    echo "===> building instant-search image locally..."
+    cd packaging
+    docker build -t instant-search .
+    eval $(minikube docker-env)
+    upload_image
+else
+    echo "===> instant-search local ok!"
+    eval $(minikube docker-env)
+    if ! docker image ls | grep instant-search >/dev/null; then
+        upload_image
+    else
+        echo "===> instant-search remote ok!"
     fi
-done
+fi
+
+http_endpoint="$(curl -sIXGET http://localhost:8080 | head -n 1 | awk '{ print $2 }')"
+if  [ "$http_endpoint" == "200" ]; then
+    endpoint_ready
+else 
+    echo "* Ensuring minikube context"
+    kubectl config use-context minikube
+    echo "===> installing instant-search-demo app in the kubernetes cluster...."
+    kubectl apply -f cluster/app.yml
+
+    echo "* forwarding ports:"
+    nohup kubectl port-forward service/instant-search 8080:8080 &
+    while [ "$(curl -sIXGET http://localhost:8080 | head -n 1 | awk '{ print $2 }')" != "200" ]; do
+        echo "waiting for the endpoint to be ready... "
+        http_endpoint="200"
+        sleep 5
+    done
+
+    if [ "$http_endpoint" == "200" ]; then
+        endpoint_ready
+    fi
+fi
